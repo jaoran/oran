c5af98e8085dfc40482b88290e83f78973699b4b
Orande <orande@me.com>
2024-11-25 21:30:19 -0500

    Add files via upload

---
+++ acc.js
@@ -0,0 +1,31 @@
+ $(document).ready(function () {
+ 	////////////////////////////////
+ 	var allContents = $(".panel");
+ 	//alert(allContents.length);
+	 var allTabs = $(".tab");
+
+	 $('#title1').addClass('currentTab');
+	 $('#content1').show();
+	 
+ 	$(".tab").click(function () {
+
+ 		let i;
+ 		for (i = 0; i < allContents.length; i++) {
+ 			//alert(allContents[i]);
+			$(allTabs[i]).removeClass("currentTab");
+ 			$(allContents[i]).slideUp(700);
+ 		}
+
+
+		let whoClicked = $(this).attr('data');
+		//alert(whoClicked);
+ 		//alert($("#"+whoClicked).css('display'));
+ 		if ($('#' + whoClicked).css('display') == 'none') {
+			$(this).addClass("currentTab");
+ 			$("#" + whoClicked).slideDown(700);
+ 		} else {
+ 			$("#" + whoClicked).slideup(700);
+ 		}
+ 	}); //end function
+
+ }); //end doc ready
